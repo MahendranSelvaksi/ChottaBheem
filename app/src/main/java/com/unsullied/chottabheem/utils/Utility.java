9c84020ef7339f3f9070bdb7f4591f502cac923e
Mahendran <mahendran209@gmail.com>
2018-08-25 13:22:26 +0530

    Committed by Mahendran
    Sample commit

--- app/src/main/java/com/unsullied/chottabheem/utils/Utility.java
+++ app/src/main/java/com/unsullied/chottabheem/utils/Utility.java
@@ -13,17 +13,6 @@ public class Utility {
     }
 
     public void hideKeyboard(Activity activity, View view) {
-      /*  InputMethodManager inputManager = (InputMethodManager) ctx
-                .getSystemService(Context.INPUT_METHOD_SERVICE);
-        Log.w(AppConstants.tag, "Called hideKayboard");
-        // check if no view has focus:
-        View v = ((Activity) ctx).getCurrentFocus();
-        if (v == null) {
-            Log.w(AppConstants.tag, "Called hideKayboard IF");
-            return;
-        }
-        inputManager.hideSoftInputFromWindow(view.getWindowToken(), 0);*/
-
         printLogcat("Called hideKeyboard");
         InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
         View v = activity.getCurrentFocus();
