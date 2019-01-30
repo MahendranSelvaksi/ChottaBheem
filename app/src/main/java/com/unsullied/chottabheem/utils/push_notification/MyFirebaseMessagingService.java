b7af02f0488fb698a377cc480935db4be3f2996a
Mahendran <mahendran209@gmail.com>
2019-01-30 20:52:58 +0530

    Committed by Mahendran
    Push notification implemented.

--- app/src/main/java/com/unsullied/chottabheem/utils/push_notification/MyFirebaseMessagingService.java
+++ app/src/main/java/com/unsullied/chottabheem/utils/push_notification/MyFirebaseMessagingService.java
@@ -1,29 +1,37 @@
 package com.unsullied.chottabheem.utils.push_notification;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
-
-import java.util.Map;
+import com.unsullied.chottabheem.R;
+import com.unsullied.chottabheem.activity.MainActivity;
 
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     public static int NOTIFICATION_ID = 100;
-
+String TAG="Success";
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
 
-        String msg =remoteMessage.getData().get("message");
-        String title=remoteMessage.getData().get("title");
+        String msg =remoteMessage.getNotification().getBody();
+        String title = remoteMessage.getNotification().getTitle();
 
-        for (Map.Entry<String, String> entry : remoteMessage.getData().entrySet()) {
+
+        /*for (Map.Entry<String, String> entry : remoteMessage.getData().entrySet()) {
             String key = entry.getKey();
             String value = entry.getValue();
-            Log.d("Success_Native", "key, " + key + " value " + value);
-        }
+            Log.w("Success_Native", "key, " + key + " value " + value);
+        }*/
 
         if (!remoteMessage.getData().isEmpty()) {
             for (String key : remoteMessage.getData().keySet()) {
@@ -32,28 +40,29 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             }
         }
 
-        Log.d("Success","Notification Message:::: "+remoteMessage.getData().keySet());
-        Log.d("Success","Notification Title:::: "+title);
+        Log.d("Success", "From: " + remoteMessage.getFrom());
+        Log.d("Success", "title: " + title);
+        Log.d("Success", "Notification Message Body: " + msg);
 
-       // sendNotification(msg);
+       sendNotification(msg,title);
     }
 
-  /*  private void sendNotification(String msg) {
+    private void sendNotification(String msg,String title) {
         //int C3NOTIFICATION_ID = 100;
        // Log.d("Check", "Comes C3 login");
         NotificationManager mNotificationManager = (NotificationManager)
                 this.getSystemService(Context.NOTIFICATION_SERVICE);
-        Intent myintent = new Intent(this, CalendarActivity.class);
-        myintent.putExtra("C3_login", getString(R.string.app_name));
+        Intent myintent = new Intent(this, MainActivity.class);
+       // myintent.putExtra("C3_login", getString(R.string.app_name));
         myintent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_SINGLE_TOP|Intent.FLAG_ACTIVITY_NEW_TASK);
         PendingIntent contentIntent = PendingIntent.getActivity(this, NOTIFICATION_ID,myintent, PendingIntent.FLAG_UPDATE_CURRENT);
         try{
             NotificationCompat.Builder mBuilder =
                     new NotificationCompat.Builder(this)
-                            .setSmallIcon(R.mipmap.reservado)
-                            .setContentTitle(getString(R.string.app_name))
+                            .setSmallIcon(R.drawable.ic_stat_name)
+                            .setContentTitle(title)
                             .setStyle(new NotificationCompat.BigTextStyle().bigText(msg))
-                            .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.reservado))
+                            .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher_new))
                             .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
                             .setContentText(msg);
             mBuilder.setContentIntent(contentIntent);
@@ -63,5 +72,5 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         }catch (Exception e){
             Log.e("Error","EEE"+e);
         }
-    }*/
+    }
 }
